c02e8ddd7c9752799c0f02b3d168a454d66c44b3
Dima <dima_sorokin54867@mail.ru>
2023-10-20 18:56:20 +0400

    AdminRentController. Получение информации по аренде по id

---
+++ Simbir.GO.Application/AdminRent/Queries/GetRent/GetRentAdminQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using ErrorOr;
+using MediatR;
+using Simbir.GO.Application.Common.Interfaces.UnitOfWork;
+using Simbir.GO.Application.Rents.Common;
+using Simbir.GO.Domain.Common.Errors;
+
+namespace Simbir.GO.Application.Rents.Queries.GetRent;
+
+internal class GetRentAdminQueryHandler : IRequestHandler<GetRentAdminQuery, ErrorOr<RentResult>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetRentAdminQueryHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<ErrorOr<RentResult>> Handle(GetRentAdminQuery request, CancellationToken cancellationToken)
+    {
+        var rent = await _unitOfWork.RentRepository.FindByIdInclude(request.RentId);
+        if (rent is null)
+            return Errors.Rent.RentDoesNotExist;
+
+        var user = await _unitOfWork.AccountRepository.FindById(request.UserId);
+        if (user is null)
+            return Errors.Account.AccountDoesNotExist;
+
+        if (!user.IsAdmin)
+            return Errors.General.OnlyForAdmin;
+
+        return new RentResult(rent.Id, rent.RenterId, rent.TransportId, rent.RentType.ToString(),
+            rent.TimeStart, rent.TimeEnd, rent.PriceOfUnit, rent.FinalPrice);
+    }
+}
---
+++ Simbir.GO.WebAPI/Controllers/AdminRentController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Simbir.GO.Application.Rents.Queries.GetRent;
+using Simbir.GO.Application.Transports.Commands.CreateTransport;
+using Simbir.GO.Domain.RentEntity;
+using Simbir.GO.Infrastructure.Identity;
+
+namespace Simbir.GO.WebAPI.Controllers;
+
+[Route("api/Admin")]
+[Authorize(Policy = IdentityPolicy.AdminPolicy)]
+public class AdminRentController : ApiContoller
+{
+    private readonly IMediator _mediator;
+
+    public AdminRentController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("Rent/{rentId}")]
+    public async Task<IActionResult> GetRent(int rentId)
+    {
+        if (await TokenIsRevokedOrAccountDoesNotExist()) return Unauthorized();
+
+        var query = new GetRentAdminQuery(GetUserId(), rentId);
+        var result = await _mediator.Send(query);
+
+        return result.Match(Ok, Problem);
+    }
+}
